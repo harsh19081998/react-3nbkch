e84fb0e6357e798fede709cb3487093a0f1afa9f
harsh19081998 <harshpatel990@gmail.com>
2022-03-11 02:20:05 +0530

    6 files modified

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -1,23 +1,87 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './contact.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFacebookMessenger,
+  faWhatsapp,
+} from '@fortawesome/free-brands-svg-icons';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(
+      'service_u1bhtpd',
+      'template_7oem8cs',
+      form.current,
+      '0G_j_LS732i7mh0Ff'
+    );
+    e.target.reset();
+  };
   return (
     <section id="contact">
       <h5>Get In Toach</h5>
       <h2>Contact Me</h2>
       <div className="container contact__container">
         <div className="contact__options">
-          <artice className="contact_option">
-            <FontAwesomeIcon icon={faEnvelope} />
+          <artice className="contact__option">
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              className="contact__option-icon"
+            />
             <h4>Email</h4>
-            <h5>harshpatel990@gmail.com</h5>
-            <a href="mailto:harshpatel990@gmail.com">Send a message</a>
+            <h5>hahp110@gmail.com</h5>
+            <a href="mailto:hahp110@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </artice>
+          <artice className="contact__option">
+            <FontAwesomeIcon
+              icon={faFacebookMessenger}
+              className="contact__option-icon"
+            />
+            <h4>Messenger</h4>
+            <h5>harsh19081998</h5>
+            <a href="https://m.me/harsh19081998" target="_blank">
+              Send a message
+            </a>
+          </artice>
+          <artice className="contact__option">
+            <FontAwesomeIcon
+              icon={faWhatsapp}
+              className="contact__option-icon"
+            />
+            <h4>WhatsApp</h4>
+            <h5>+91 9913711695</h5>
+            <a
+              href="https://api.whatsApp.com/send?phone+919913711695"
+              target="_blank"
+            >
+              Send a message
+            </a>
           </artice>
         </div>
-        <form action=""></form>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
   );
--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,7 +1,53 @@
 import React from 'react';
 import './footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        HARSH PATEL
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a
+          href="https://www.facebook.com/profile.php?id=100027504952258"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faFacebook} />
+        </a>
+        <a href="https://instagram.com/harsh19081998" target="_blank">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a href="https://www.twitter.com/harshpatel1999" target="_blank">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; HARSH Patel.All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
